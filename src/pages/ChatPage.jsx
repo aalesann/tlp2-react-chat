b998d506d0c638994f220bb8499ea964aa9fa2e6
aalesann <alejandrosanabria74@gmail.com>
2023-11-17 16:43:30 -0300

    Push que pidio Leo

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';// Asegúrate de importar los estilos de Bootstrap
-import { ChatSideBar } from '../components/ChatSideBar';
-import { ChatListMessage } from '../components/ChatListMessage';
+import { useState, useEffect, useContext } from 'react';// Asegúrate de importar los estilos de Bootstrap
+import { ChatContext } from '../context/ChatProvider';
+
 
 export const ChatPage = () => {
 
   const [newMessage, setNewMessage] = useState('');
+  const { chatState } = useContext(ChatContext);
 
 
   // Esta función podría ser modificada para enviar el mensaje a tu backend o WebSocket
@@ -30,9 +31,27 @@ export const ChatPage = () => {
     <div className="container my-4">
       <div className="row">
         {/* Lista de Usuarios */}
-        <ChatSideBar />
+        <div className="col-5">
+          <ul className='list-unstyled'>
+            {
 
-        <ChatListMessage />
+              chatState.usuarios.map( user => (
+                <li key={user.uid}>
+                    <span className={`badge text-bg-${user.online ? 'success' : 'danger'}`}> o &nbsp;</span> &nbsp; { user.username }
+                </li>
+              ))
+
+            }
+          </ul>
+
+        </div>
+
+
+        {/* Lista de Mensajes */}
+        <div className="col-7"></div>
+
+
+
       </div>
     </div>
 
